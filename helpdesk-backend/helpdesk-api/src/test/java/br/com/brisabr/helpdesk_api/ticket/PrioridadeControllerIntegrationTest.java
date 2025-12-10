1023fdf7a63d8258416d002776bd36a85f7d9f3c
HelpDesk Developer <helpdesk@brisabr.com>
2025-12-10 13:02:22 -0300

    Fix: Use TestPropertySource to force H2 database in tests

--- helpdesk-backend/helpdesk-api/src/test/java/br/com/brisabr/helpdesk_api/ticket/PrioridadeControllerIntegrationTest.java
+++ helpdesk-backend/helpdesk-api/src/test/java/br/com/brisabr/helpdesk_api/ticket/PrioridadeControllerIntegrationTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class PrioridadeControllerIntegrationTest {
 
     @Autowired
