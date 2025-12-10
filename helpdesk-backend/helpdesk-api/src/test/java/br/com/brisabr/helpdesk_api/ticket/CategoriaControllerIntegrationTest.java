0086e3aa802d6f4fe5d6b823fde91e77e3a11b61
HelpDesk Developer <helpdesk@brisabr.com>
2025-12-10 13:04:58 -0300

    Fix: Add inline H2 properties to test classes

--- helpdesk-backend/helpdesk-api/src/test/java/br/com/brisabr/helpdesk_api/ticket/CategoriaControllerIntegrationTest.java
+++ helpdesk-backend/helpdesk-api/src/test/java/br/com/brisabr/helpdesk_api/ticket/CategoriaControllerIntegrationTest.java
@@ -23,7 +23,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-@TestPropertySource(locations = "classpath:application-test.properties")
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
+})
 class CategoriaControllerIntegrationTest {
 
     @Autowired
